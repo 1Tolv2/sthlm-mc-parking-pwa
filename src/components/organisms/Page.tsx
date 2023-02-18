0cf47ba0caa0086cf42fbf3f14a4f1dc8ed1ef47
1Tolv2 <sofiaq1@gmail.com>
2023-02-18 21:35:51 +0100

    refactored location button

--- src/components/organisms/Page.tsx
+++ src/components/organisms/Page.tsx
@@ -30,8 +30,8 @@ export const Page = ({
     <Layout>
       <AddressSearch />
       {children}
-      <div className="flex flex-col gap-sm">
-        <LocationButton icon="locationOff" />
+      <div className="flex flex-col gap-2">
+        <LocationButton />
         <Navigation />
       </div>
     </Layout>
