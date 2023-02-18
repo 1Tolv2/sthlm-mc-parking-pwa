68a85cb91cf8bd4941a3d17fb9a0416b18b169aa
1Tolv2 <sofiaq1@gmail.com>
2023-02-18 20:00:21 +0100

    extended deafault theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#e6edff",
+          200: "#ccdaff",
+          300: "#b3c7ff",
+          400: "#80a2ff",
+          500: "#3F75FF",
+          600: "#1a5bff",
+          700: "#0041e6",
+          800: "#002480",
+          900: "#00164d",
+          DEFAULT: "#3F75FF",
+        },
+        neutral: "#D9D9D9",
+        black: "#353535",
+      },
+      spacing: {
+        sm: "5px",
+        md: "20px",
+        lg: "30px",
+        xl: "40px",
+      },
+    },
   },
   plugins: [],
 };
