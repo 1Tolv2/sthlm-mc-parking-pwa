a3ea0398620f70b8d7ae2b0aca6cfac2bdfda156
1Tolv2 <sofiaq1@gmail.com>
2023-02-19 16:06:07 +0100

    split up properties

--- types/FeatureItem.ts
+++ types/FeatureItem.ts
@@ -1,5 +1,25 @@
 import { CoordinateItem } from "./CoordinateItem";
 import { Weekday } from ".";
+type Properties = {
+  FID: number;
+  FEATURE_OBJECT_ID: number;
+  FEATURE_VERSION_ID: number;
+  EXTENT_NO: 1;
+  VALID_FROM: string;
+  START_TIME: number;
+  END_TIME: number;
+  START_WEEKDAY: Weekday;
+  CITATION: string;
+  STREET_NAME: string;
+  CITY_DISTRICT: string;
+  PARKING_DISTRICT: string;
+  ADDRESS: string;
+  VF_METER: number;
+  VF_PLATS_TYP: "Reserverad p-plats motorcykel";
+  OTHER_INFO: string;
+  RDT_URL: string;
+  PARKING_RATE: string;
+};
 
 export interface FeatureItem {
   type: "Feature";
@@ -9,24 +29,5 @@ export interface FeatureItem {
     coordinates: CoordinateItem[];
   };
   geometry_name: "GEOMETRY";
-  properties: {
-    FID: number;
-    FEATURE_OBJECT_ID: number;
-    FEATURE_VERSION_ID: number;
-    EXTENT_NO: 1;
-    VALID_FROM: string;
-    START_TIME: number;
-    END_TIME: number;
-    START_WEEKDAY: Weekday;
-    CITATION: string;
-    STREET_NAME: string;
-    CITY_DISTRICT: string;
-    PARKING_DISTRICT: string;
-    ADDRESS: string;
-    VF_METER: number;
-    VF_PLATS_TYP: "Reserverad p-plats motorcykel";
-    OTHER_INFO: string;
-    RDT_URL: string;
-    PARKING_RATE: string;
-  };
+  properties: Properties;
 }
