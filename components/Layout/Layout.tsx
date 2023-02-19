6aaab0b9304cff9c2d25ab11ee0011135ad4648c
1Tolv2 <sofiaq1@gmail.com>
2023-02-19 12:44:15 +0100

    added typing to context

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,15 +1,33 @@
 import React, { PropsWithChildren, createContext, useState } from "react";
 import { FeatureItem } from "../../types";
-const ParkingContext = createContext({});
+
+interface ModalCtx {
+  currentParkingSpot: FeatureItem | null;
+  setCurrentParkingSpot: React.Dispatch<
+    React.SetStateAction<FeatureItem | null>
+  >;
+  parkingSpots: FeatureItem[];
+  setParkingSpots: React.Dispatch<React.SetStateAction<FeatureItem[]>>;
+}
+const ParkingContext = createContext<ModalCtx>({} as ModalCtx);
 
 export default function Layout({
   children,
 }: PropsWithChildren<Record<string, unknown>>): JSX.Element {
   const [parkingSpots, setParkingSpots] = useState<FeatureItem[]>([]);
+  const [currentParkingSpot, setCurrentParkingSpot] =
+    useState<FeatureItem | null>(null);
 
   return (
-    <ParkingContext.Provider value={{ parkingSpots, setParkingSpots }}>
-      <main className="flex flex-col justify-between h-screen w-screen bg-neutral p-md">
+    <ParkingContext.Provider
+      value={{
+        parkingSpots,
+        setParkingSpots,
+        currentParkingSpot,
+        setCurrentParkingSpot,
+      }}
+    >
+      <main className="relative h-screen w-screen bg-neutral p-md">
         {children}
       </main>
     </ParkingContext.Provider>
