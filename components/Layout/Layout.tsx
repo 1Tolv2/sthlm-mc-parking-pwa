fa1a07535d697a526f6abed72ef44e008c1968ab
1Tolv2 <sofiaq1@gmail.com>
2023-02-19 12:27:00 +0100

    added context

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,19 @@
-import React, { PropsWithChildren } from "react";
+import React, { PropsWithChildren, createContext, useState } from "react";
+import { FeatureItem } from "../../types";
+const ParkingContext = createContext({});
 
 export default function Layout({
   children,
 }: PropsWithChildren<Record<string, unknown>>): JSX.Element {
+  const [parkingSpots, setParkingSpots] = useState<FeatureItem[]>([]);
+
   return (
-    <main className="flex flex-col justify-between h-screen w-screen bg-neutral p-md">
-      {children}
-    </main>
+    <ParkingContext.Provider value={{ parkingSpots, setParkingSpots }}>
+      <main className="flex flex-col justify-between h-screen w-screen bg-neutral p-md">
+        {children}
+      </main>
+    </ParkingContext.Provider>
   );
 }
+
+export { ParkingContext };
