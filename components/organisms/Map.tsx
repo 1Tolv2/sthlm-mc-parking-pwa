4dabdfedb161e2c880955bad5957bd98d933eb2f
1Tolv2 <sofiaq1@gmail.com>
2023-02-20 22:42:29 +0100

    Added Map component

---
+++ components/organisms/Map.tsx
@@ -0,0 +1,9 @@
+import Reactfrom "react";
+
+type Props = {};
+
+const Map = (props: Props) => {
+  return <div>Map</div>;
+};
+
+export default Map;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import LocationButton from "../components/molecules/LocationButton";
 import AddressSearch from "../components/organisms/AddressSearch";
 import Navigation from "../components/organisms/Navigation";
 import ParkingLocations from "../components/molecules/ParkingLocations";
+import Map from "../components/organisms/Map";
 
 export default function Page() {
   return (
@@ -18,6 +19,7 @@ export default function Page() {
         <title>Sofia Johnsson S.</title>
       </Head>
       <Layout>
+        <Map />
         <AddressSearch />
         <ParkingLocations />
         <div className="fixed bottom-md left-1/2 -translate-x-1/2 flex flex-col gap-2 w-full">
