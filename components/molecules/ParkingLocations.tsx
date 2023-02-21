943aa5738e4525b0fe939a0ff804883c7eab77e0
1Tolv2 <sofiaq1@gmail.com>
2023-02-21 22:15:55 +0100

    Renders locations on the map

--- components/molecules/ParkingLocations.tsx
+++ components/molecules/ParkingLocations.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react";
+import { Marker } from "@react-google-maps/api";
 import { ParkingContext } from "../Layout/Layout";
 import { getParkingSpots } from "../api";
+
 import ParkingDetailModal from "./ParkingDetailModal";
 
 const ParkingLocations = () => {
@@ -20,6 +22,7 @@ const ParkingLocations = () => {
         data.features[1],
         data.features[2],
         data.features[3],
+        data.features[4],
       ]);
     }
   };
@@ -29,28 +32,32 @@ const ParkingLocations = () => {
   }, []);
 
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-1/2 z-50  w-fit h-fit">
-      <ul className="grid grid-cols-2 gap-md">
-        {parkingSpots?.map((item) => (
+    <ul>
+      {parkingSpots?.map((item) => {
+        const position: any = {
+          lng: item?.geometry?.coordinates[0][0],
+          lat: item?.geometry?.coordinates[0][1],
+        };
+        return (
           <li
             key={item.id}
             id={item.id}
+            className="relative"
             onClick={(e) =>
               setCurrentParkingSpot(
                 parkingSpots?.find((item) => item.id === e.currentTarget.id) ||
                   null
               )
             }
-            className="relative"
           >
+            <Marker position={position} />
             {currentParkingSpot && currentParkingSpot.id === item.id && (
               <ParkingDetailModal data={item} />
             )}
-            <span className="block w-[20px] h-[20px] bg-primary-500 rounded-full cursor-pointer drop-shadow" />
           </li>
-        ))}
-      </ul>
-    </div>
+        );
+      })}
+    </ul>
   );
 };
 
