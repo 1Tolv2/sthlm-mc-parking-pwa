f1e61d38ef073d3dbe9064bbee4e689d3646d90e
1Tolv2 <sofiaq1@gmail.com>
2023-02-21 22:15:18 +0100

    Moved locations component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Layout from "../components/Layout/Layout";
 import LocationButton from "../components/molecules/LocationButton";
 import AddressSearch from "../components/organisms/AddressSearch";
 import Navigation from "../components/organisms/Navigation";
-import ParkingLocations from "../components/molecules/ParkingLocations";
 import Map from "../components/molecules/Map";
 
 export default function Page() {
@@ -22,7 +21,6 @@ export default function Page() {
         <Map />
         <div className="w-screen h-screen p-md">
           <AddressSearch />
-          <ParkingLocations />
           <div className="fixed bottom-md left-1/2 -translate-x-1/2 flex flex-col gap-2 w-full">
             <LocationButton />
             <Navigation />
