9c7087cb4b2679089a7593353680b52e1edf9abc
1Tolv2 <sofiaq1@gmail.com>
2023-03-05 16:35:21 +0100

    added manifest and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,12 @@ export default function Page() {
       <Head>
         <meta charSet="UTF-8" />
         <meta
-          name="Sök efter mc parkering i Stockholm"
-          content="STHLM MC parkering"
-        ></meta>
+          name="description"
+          content="Hitta mc parkering i stockholm"
+          key="desc"
+        />
         <title>STHLM MC Parking</title>
+        <link rel="manifest" href="./manifest.json"></link>
       </Head>
       <Layout>
         <Content />
