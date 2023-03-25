d4ca1b58c85909a825bd74c5bef99684412e745d
1Tolv2 <sofiaq1@gmail.com>
2023-03-25 22:47:11 +0100

    Changed texting

--- components/molecules/loadingComponents/LoadingScreen.tsx
+++ components/molecules/loadingComponents/LoadingScreen.tsx
@@ -5,9 +5,14 @@ type Props = {};
 
 const LoadingScreen = (props: Props) => {
   return (
-    <div className="absolute flex gap-10 flex-col justify-center items-center w-full h-full bg-primary z-[100]">
+    <div className="absolute flex gap-10 flex-col justify-center items-center w-full h-full bg-primary z-[100] text-center">
+      <h1 className="text-[24px] sm:text-[40px] text-white">
+        STHLM MC Parkering
+      </h1>
       <LoadingAnimation />
-      <p className="text-xl text-white">Laddar in alla parkeringar</p>
+      <p className="text-sm sm:text-xl text-white">
+        Laddar in alla parkeringar
+      </p>
     </div>
   );
 };
