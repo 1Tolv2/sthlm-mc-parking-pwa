fd3c1f614268b8746cd2c14b779eb1a6447c7515
1Tolv2 <sofiaq1@gmail.com>
2023-03-26 17:17:00 +0200

    changed font

--- components/molecules/loadingComponents/LoadingScreen.tsx
+++ components/molecules/loadingComponents/LoadingScreen.tsx
@@ -6,11 +6,11 @@ type Props = {};
 const LoadingScreen = (props: Props) => {
   return (
     <div className="absolute flex gap-10 flex-col justify-center items-center w-full h-full bg-primary z-[100] text-center">
-      <h1 className="text-[24px] sm:text-[40px] text-white">
+      <h1 className="font-tratex text-[24px] sm:text-[40px] text-white">
         STHLM MC Parkering
       </h1>
       <LoadingAnimation />
-      <p className="text-sm sm:text-xl text-white">
+      <p className="font-tratex text-sm sm:text-xl text-white">
         Laddar in alla parkeringar
       </p>
     </div>
