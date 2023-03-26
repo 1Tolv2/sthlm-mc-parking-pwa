97b2c2498181a3567ee64286de8bd990890da31c
1Tolv2 <sofiaq1@gmail.com>
2023-03-26 17:46:21 +0200

    removed old code

--- components/organisms/Content.tsx
+++ components/organisms/Content.tsx
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAppContext } from "../../context/AppContext";
-import { useMapContext } from "../../context/MapContext";
-import { FeatureItem, CoordinateItem } from "../../types";
 import Map from "../molecules/Map";
 import TopNavigation from "./TopNavigation";
 import BottomNavigation from "./BottomNavigation";
@@ -11,22 +9,7 @@ import LoadingModal from "../molecules/loadingComponents/LoadingModal";
 type Props = {};
 
 const Content = (props: Props) => {
-  const { isInitialLoading, isLoading, setIsInitialLoading, setIsLoading } =
-    useAppContext();
-
-  const { zoom, setZoom, center, setCenter } = useMapContext();
-  // const [zoom, setZoom] = useState(11);
-  // const [center, setCenter] = useState<CoordinateItem | null>({
-  //   lat: 59.31323345086049,
-  //   lng: 18.07502720995736,
-  // });
-
-  const [parkingSpots, setParkingSpots] = useState<FeatureItem[]>([]);
-  const [targetedParkingSpot, setTargetedParkingSpot] =
-    useState<FeatureItem | null>(null);
-  const [currentLocation, setCurrentLocation] = useState<CoordinateItem | null>(
-    null
-  );
+  const { isInitialLoading, isLoading } = useAppContext();
 
   return (
     <>
