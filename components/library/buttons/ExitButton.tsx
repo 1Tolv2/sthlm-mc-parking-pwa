f8c272ee0f5431b6bfdb075022f853d5960f04b9
1Tolv2 <sofiaq1@gmail.com>
2023-04-06 18:10:10 +0200

    added color prop

--- components/library/buttons/ExitButton.tsx
+++ components/library/buttons/ExitButton.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 type Props = {
+  color?: string;
   handleOnClick: () => void;
 };
 
-const ExitButton = ({ handleOnClick }: Props) => {
+const ExitButton = ({ color = "neutral-600", handleOnClick }: Props) => {
   return (
     <div className="cursor-pointer mx-auto w-6 h-6" onClick={handleOnClick}>
-      <div className="relative top-2 w-6 h-0.5 rotate-45 bg-neutral-600" />
-      <div className="relative top-1.5 w-6 h-0.5 -rotate-45 bg-neutral-600" />
+      <div className={`relative top-2 w-6 h-0.5 rotate-45 bg-${color}`} />
+      <div className={`relative top-1.5 w-6 h-0.5 -rotate-45 bg-${color}`} />
     </div>
   );
 };
