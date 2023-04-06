d060d3a9c637ca7759028eac32f9a249db6e9cb5
1Tolv2 <sofiaq1@gmail.com>
2023-04-06 19:23:35 +0200

    changed param attachment

--- pages/api/streets/streetNames.tsx
+++ pages/api/streets/streetNames.tsx
@@ -6,9 +6,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const searchParam = req.query.search as string;
+  console.log("SEARCH", searchParam);
+  const params = new URLSearchParams();
+  params.append("streetNamePattern", searchParam + "*");
+  params.append("apiKey", process.env.NEXT_APP_TRAFIKVERKET_API_KEY || "");
 
-  const url = `${process.env.NEXT_APP_LV_API_URL}/GetStreetNames?apiKey=${process.env.NEXT_APP_TRAFIKVERKET_API_KEY}&streetNamePattern=${searchParam}*`;
-  const { data } = await axios.get(url);
+  const url = `${process.env.NEXT_APP_LV_API_URL}/GetStreetNames`;
+  const { data } = await axios.get(url, { params });
 
   if (data.length === 0) {
     const url = `${process.env.NEXT_APP_LV_API_URL}/GetStreetNamesWithinLevenshteinDistance?apiKey=${process.env.NEXT_APP_TRAFIKVERKET_API_KEY}&streetName=${searchParam}&maxDistance=3`;
