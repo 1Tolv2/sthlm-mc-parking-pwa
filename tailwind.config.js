e425e80d99e45a349e04721f187052b6b2f65abe
1Tolv2 <sofiaq1@gmail.com>
2023-04-07 23:05:00 +0200

    fixed faulty

--- tailwind.config.js
+++ tailwind.config.js
@@ -38,7 +38,7 @@ module.exports = {
   plugins: [],
   safelist: [
     // allows all widths and heights
-    { pattern: /(w|h)-\[*\w+\]*/ },
+    { pattern: /(w|h)-\[*.+\]*/ },
     // allows a paddings and margins
     { pattern: /(p|m)(t|b|r|l|)*-\[*\w+\]*/ },
     { pattern: /bg-(neutral)-\d{3}/ },
