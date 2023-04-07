e08687d6d1fbf20275a2348bbfeb7727bbf93594
1Tolv2 <sofiaq1@gmail.com>
2023-04-07 22:01:29 +0200

    changed to allow url bar to not overlap

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({
   children,
 }: PropsWithChildren<Record<string, unknown>>): JSX.Element {
   return (
-    <main className="relative flex flex-col justify-between h-[100svh] w-screen p-md bg-white">
+    <main className="relative flex flex-col justify-between h-[100dvh] w-screen p-md bg-white">
       {children}
     </main>
   );
