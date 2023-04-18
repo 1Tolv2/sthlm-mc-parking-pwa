f2fea9ad338df5207d5f0a07cf67e4c6e34aee48
1Tolv2 <sofiaq1@gmail.com>
2023-04-18 23:26:22 +0200

    fixed faulty slider

--- components/library/ParkingListSlider.tsx
+++ components/library/ParkingListSlider.tsx
@@ -8,9 +8,7 @@ const ParkingListSlider = () => {
   const { parkingSpots, targetedParkingSpot } = useParkingContext();
   console.log("TARGET", parkingSpots, "clicked", targetedParkingSpot);
 
-  return !targetedParkingSpot && parkingSpots.length <= 10 ? (
-    <></>
-  );
+  return !targetedParkingSpot && parkingSpots.length <= 10 && <></>;
 };
 
 export default ParkingListSlider;
