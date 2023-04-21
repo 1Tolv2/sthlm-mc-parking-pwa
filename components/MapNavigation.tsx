7e682d53819b909e7969d68ffc5b253eac8728ed
1Tolv2 <sofiaq1@gmail.com>
2023-04-21 13:33:23 +0200

    changed size

--- components/MapNavigation.tsx
+++ components/MapNavigation.tsx
@@ -6,7 +6,7 @@ import ParkingListSlider from "./library/ParkingListSlider";
 const MapNavigation = () => {
   return (
     <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex flex-col gap-2 w-full h-fit max-w-[500px] md:max-w-none p-md z-50 pointer-events-none">
-      <div className="relative flex justify-between z-[51]">
+      <div className="relative w-full flex justify-between z-[51]">
         <ShowAllButton />
         <LocationButton />
       </div>
