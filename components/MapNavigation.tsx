0084b15c802b6f6d97570c97b22e18350b613c48
1Tolv2 <sofiaq1@gmail.com>
2023-04-22 13:58:35 +0200

    added parkingListSlider

--- components/MapNavigation.tsx
+++ components/MapNavigation.tsx
@@ -5,8 +5,8 @@ import ParkingListSlider from "./library/ParkingListSlider";
 
 const MapNavigation = () => {
   return (
-    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex flex-col gap-2 w-full h-fit max-w-[500px] md:max-w-none p-md z-50 pointer-events-none">
-      <div className="relative w-full flex justify-between z-[51]">
+    <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex flex-col gap-2 w-full h-fit py-md z-50 pointer-events-none">
+      <div className="relative px-md w-full flex justify-between max-w-[500px] md:max-w-none z-[51]">
         <ShowAllButton />
         <LocationButton />
       </div>
