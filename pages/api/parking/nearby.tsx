1bba015dcab9fe79923a86b3587700ab29a0b5d0
1Tolv2 <sofiaq1@gmail.com>
2023-05-02 13:56:49 +0200

    only allow specific method

--- pages/api/parking/nearby.tsx
+++ pages/api/parking/nearby.tsx
@@ -5,23 +5,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { coordinates } = req.body;
+  if (req.method === "POST") {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { coordinates } = req.body;
 
-  const radius = "&radius=100";
-  const url = `${process.env.NEXT_APP_OPEN_PARKING_API_URL}/within?lat=${coordinates.latitude}&lng=${coordinates.longitude}&outputFormat=json&apiKey=${process.env.NEXT_APP_OPEN_PARKING_API_KEY}`;
+    const radius = "&radius=100";
+    const url = `${process.env.NEXT_APP_OPEN_PARKING_API_URL}/within?lat=${coordinates.latitude}&lng=${coordinates.longitude}&outputFormat=json&apiKey=${process.env.NEXT_APP_OPEN_PARKING_API_KEY}`;
 
-  const response = await axios.get(url + radius);
-  const { data } = response;
+    const response = await axios.get(url + radius);
+    const { data } = response;
 
-  if (data.features.length > 0) {
-    return res.status(200).json(data);
-  } else {
-    const { data } = await axios.get(url + "&radius=200");
-    if (data) {
+    if (data.features.length > 0) {
       return res.status(200).json(data);
     } else {
-      return res.status(404).json({ message: "No parking spots found" });
+      const { data } = await axios.get(url + "&radius=200");
+      if (data.features.length > 0) {
+        return res.status(200).json(data);
+      } else {
+        return res.status(404).json({ message: "No parking spots found" });
+      }
     }
+  } else {
+    return res.status(404).json({ message: "Method not available" });
   }
 }
