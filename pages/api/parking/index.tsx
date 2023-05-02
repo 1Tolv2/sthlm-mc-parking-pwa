1879b0fc3d74264142dcb801bd0859588d0eed4e
1Tolv2 <sofiaq1@gmail.com>
2023-05-02 12:45:46 +0200

    prevent wrong method to access endpoint

--- pages/api/parking/index.tsx
+++ pages/api/parking/index.tsx
@@ -5,7 +5,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const url = `${process.env.NEXT_APP_TRAFIKVERKET_API_URL}/all?outputFormat=json&apiKey=${process.env.NEXT_APP_TRAFIKVERKET_API_KEY}`;
-  const { data } = await axios.get(url);
-  return res.status(200).json(data);
+  if (req.method === "GET") {
+    const url = `${process.env.NEXT_APP_OPEN_PARKING_API_URL}/all?outputFormat=json&apiKey=${process.env.NEXT_APP_OPEN_PARKING_API_KEY}`;
+    const response = await axios.get(url);
+    const { data } = response;
+
+    return res.status(200).json(data);
+  } else {
+    return res.status(404).json({ message: "Method not available" });
+  }
 }
--- pages/api/parking/street.tsx
+++ pages/api/parking/street.tsx
@@ -5,8 +5,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const searchParam = req.query.search;
-  const url = `${process.env.NEXT_APP_OPEN_PARKING_API_URL}/street/${searchParam}?outputFormat=json&apiKey=${process.env.NEXT_APP_OPEN_PARKING_API_KEY}`;
-  const { data } = await axios.get(url);
-  return res.status(200).json(data);
+  if (req.method === "GET") {
+    const searchParam = req.query.search;
+    const url = `${process.env.NEXT_APP_OPEN_PARKING_API_URL}/street/${searchParam}?outputFormat=json&apiKey=${process.env.NEXT_APP_OPEN_PARKING_API_KEY}`;
+    const { data } = await axios.get(url);
+    return res.status(200).json(data);
+  } else {
+    return res.status(404).json({ message: "Method not available" });
+  }
 }
