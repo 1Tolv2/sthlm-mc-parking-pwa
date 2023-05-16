230264ed340ca9fae2048e613d1f496afd19a3c5
1Tolv2 <sofiaq1@gmail.com>
2023-05-16 10:43:14 +0200

    moved nav function to map

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+
+import { getNearbyParkingSpots } from "./api";
 import { useAppContext } from "../context/AppContext";
 import { useParkingContext } from "../context/ParkingContext";
-import { FeatureItem } from "../types";
-import Map from "./library/map/Map";
+import { useModalContext } from "../context/ModalContext";
+
+import { FeatureItem, CoordinateItem } from "../types";
+
 import TopNavigation from "./TopNavigation";
 import MapNavigation from "./MapNavigation";
 import LoadingScreen from "./library/loading/LoadingScreen";
 import ParkingDetailModal from "./library/ParkingDetailModal";
+import { useMapContext } from "../context/MapContext";
 
 type Props = {
   data: FeatureItem[];
@@ -16,11 +21,52 @@ type Props = {
 const Content = ({ data }: Props) => {
   const [isSearching, setIsSearching] = useState<boolean>(false);
 
-  const { isInitialLoading, isLoading } = useAppContext();
-  const { setParkingSpots } = useParkingContext();
+  const { isInitialLoading, isLoading, setIsLoading, setIsInitialLoading } =
+    useAppContext();
+  const { setMapView, mapView } = useMapContext();
+  const { setParkingSpots, setCurrentLocation } = useParkingContext();
+  const { setModalContent } = useModalContext();
+
+  const handleNearbyParkingSpots = async (
+    position: GeolocationPosition
+  ): Promise<void> => {
+    setIsLoading(true);
+    const locationBasedData = await getNearbyParkingSpots({
+      lng: position.coords.longitude,
+      lat: position.coords.latitude,
+    } as CoordinateItem);
+
+    if (!locationBasedData.features || locationBasedData.features?.length === 0)
+      setModalContent("Inga parkeringar i närheten");
+
+    setMapView({
+      zoom: 16,
+      center: {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      },
+    });
+    setCurrentLocation({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    });
+
+    if (locationBasedData.features?.length > 0) {
+      setParkingSpots(locationBasedData.features);
+    } else {
+      setParkingSpots(data);
+    }
+
+    setIsLoading(false);
+    setIsInitialLoading(false);
+  };
 
   useEffect(() => {
-    if (data && data.length > 0) setParkingSpots(data);
+    navigator.geolocation.getCurrentPosition(handleNearbyParkingSpots, () => {
+      if (data && data.length > 0) setParkingSpots(data);
+      setIsLoading(false);
+      setIsInitialLoading(false);
+    });
   }, []);
 
   const DynamicMap = dynamic(() => import("./library/map/Map"), { ssr: false });
@@ -29,15 +75,13 @@ const Content = ({ data }: Props) => {
     <>
       {isLoading && isInitialLoading && <LoadingScreen />}
       <TopNavigation setIsSearching={setIsSearching} />
-
+      <ParkingDetailModal />
       <DynamicMap>
         <MapNavigation
           isSearching={isSearching}
           setIsSearching={setIsSearching}
         />
       </DynamicMap>
-
-      <ParkingDetailModal />
     </>
   );
 };
