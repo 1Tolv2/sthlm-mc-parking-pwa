a85131de85d415a2b34720481f35970ef8fa0f12
Sofia Johnsson <sofiaq1@gmail.com>
2023-09-29 13:21:28 +0200

    doesn't display old features

--- utils/pruneFeatures.ts
+++ utils/pruneFeatures.ts
@@ -3,13 +3,16 @@ import { FeatureItem } from "../types/FeatureItem";
  * @param data Feature list
  * @description Removes duplicate features and reported non-existing features.
  */
-const pruneFeatures = (data: FeatureItem[]) => {
-  console.log("Begin pruning features", data.length);
-  const modifiedData: FeatureItem[] = [];
 
-  data.forEach((feature: any) => {
-    // If a feature with the same address doesn't exist in the new array or has name <Adress saknas> it's added to the array.
-    if (
+const deprecatedFeatures: string[] = ["LTFR_P_MOTORCYKEL.2390318"];
+
+const pruneFeatures = (data: FeatureItem[]) => {
+  const modifiedData: FeatureItem[] = [];
+  data.forEach((feature: FeatureItem) => {
+    if (deprecatedFeatures.includes(feature.id)) {
+      return;
+    } else if (
+      // If a feature with the same address doesn't exist in the new array or has name <Adress saknas> it's added to the array.
       !modifiedData.find(
         (item) => item.properties.ADDRESS === feature.properties.ADDRESS
       )
@@ -19,8 +22,6 @@ const pruneFeatures = (data: FeatureItem[]) => {
       modifiedData.push(feature);
     }
   });
-
-  console.log("Pruning finished", modifiedData.length);
 
   return modifiedData;
 };
